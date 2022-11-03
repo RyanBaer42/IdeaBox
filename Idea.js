a26174a227615ca915ab22dc0928ab3ce0460faa
Blanche Haddad <blancherachel@gmail.com>
2022-11-03 16:31:46 -0600

    Update the updateIdea method in our Idea class

--- Idea.js
+++ Idea.js
@@ -7,7 +7,6 @@ class Idea {
     }
 
   updateIdea() {
-    //toggle between the two star images, image source white and red,
-    // will update the star value to true or false
+    this.starred = !this.starred;
   }
 }
